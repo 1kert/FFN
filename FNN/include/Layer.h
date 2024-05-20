5e791fa3af544588e5688e9575f30866113445a2
1kert <kertmand1@gmail.com>
2024-05-21 02:49:19 +0300

    calculate error

--- FNN/include/Layer.h
+++ FNN/include/Layer.h
@@ -41,15 +41,6 @@ public:
         }
         return output;
     }
-
-    double calculateError(std::vector<double> actual, std::vector<double> expected)
-    {
-        if(actual.size() != expected.size()) throw std::length_error("actual and expected not the same size");
-
-        double sum = 0;
-        for(size_t i = 0; i < actual.size(); i++) sum += std::pow(actual[i] - expected[i], 2);
-        return sum /= actual.size();
-    }
 };
 
 #endif
--- FNN/include/Network.h
+++ FNN/include/Network.h
@@ -31,6 +31,15 @@ public:
 		}
 		return inputs;
 	}
+
+	static double calculateError(std::vector<double> actual, std::vector<double> expected)
+    {
+        if(actual.size() != expected.size()) throw std::length_error("actual and expected not the same size");
+
+        double sum = 0;
+        for(size_t i = 0; i < actual.size(); i++) sum += std::pow(actual[i] - expected[i], 2);
+        return sum /= actual.size();
+    }
 };
 
 #endif
