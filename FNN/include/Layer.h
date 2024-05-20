48fef9a53b08efa2500aa6e9de8080ad9bda76eb
1kert <kertmand1@gmail.com>
2024-05-20 23:54:28 +0300

    weights and biases init

--- FNN/include/Layer.h
+++ FNN/include/Layer.h
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <vector>
+#include <random>
 
 class Layer
 {
@@ -12,8 +13,16 @@ public:
 	Layer(int size, int inputs)
 	{
 		weights = std::vector<std::vector<double>>(size);
-		for (int i = 0; i < size; i++) weights[i] = std::vector<double>(inputs);
+        std::random_device rd;
+        std::default_random_engine engine(rd());
+        std::uniform_real_distribution distr(0.0, 1.0);
+		for (int i = 0; i < size; i++) 
+        {
+            weights[i] = std::vector<double>(inputs);
+            for(int j = 0; j < inputs; j++) weights[i][j] = distr(engine);
+        }
 		biases = std::vector<double>(size);
+        for(int i = 0; i < size; i++) biases[i] = distr(engine);
 	}
 };
 
